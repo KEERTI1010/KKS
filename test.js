1c9596586363bb4c7f8f45c0d0e016c55544d156
KEERTI1010 <shetkeerti6@gmail.com>
2025-11-24 19:13:02 +0530

    adding new file test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+console.log ("Keerti")
+const accountId = "12345";
+let accountname = "Keerti";
+var account_type = "savings";
+console.log (accountId);
+console.log (accountname);
+console.log (account_type); 
+
+accountname = "Reddy";
+console.log (accountname);
+account_type = "current";
+console.log (account_type);
