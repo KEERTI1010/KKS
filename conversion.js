434b51a80076b6162ee59d0e163eca81eb1ea708
KEERTI1010 <shetkeerti6@gmail.com>
2025-11-25 18:54:34 +0530

    conversion of data types

---
+++ conversion.js
@@ -0,0 +1,70 @@
+//let score = 33
+
+//console.log(typeof score); //number
+//console.log(typeof(score));//number
+
+//let score = '33'
+
+//console.log(typeof score); //string
+//console.log(typeof(score));//string
+
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber); //33
+
+//let score = '33abcc'
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber); //NaN
+
+//let score = null
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber); //0
+
+//let score = undefined
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber); //NaN
+
+//let score = true;
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber); //1
+
+//let score = false;
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber); //0
+
+//let score = "KARTHIK"
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber); //NaN
+
+
+
+//soo
+//"33" => 33
+//"33abcc" => NaN
+//null => 0 
+//true => 1 
+//false=0
+//undefined => NaN
+
+
+let isLoggedIn =""
+let booleanIsLoggened = Boolean(isLoggedIn);
+//console.log(booleanIsLoggened); 
+
+//"" => false
+//" " => true
+//"keerti" => true
+
+let someNumber = 43
+let stringNumber = String(someNumber);
+console.log(stringNumber);//43
+console.log(typeof stringNumber);//string
+//43 => "43"
+
+
